c599daf4660422246c8cd797ab79ba33aff8c411
Lucas Duncan <lucasdonuts@gmail.com>
2022-12-20 17:48:26 -0600

    All tests passing

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here
-    pass
+    for i in range(10, 0, -1):
+        print(i)
+    
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here
-    pass
+    return [num * num for num in int_list]
 
 def fizzbuzz():
-    # code goes here
-    pass
+    for i in range(1, 101):
+        string = ''
+
+        if i % 5 != 0 and i % 3 != 0:
+            print(i)
+            continue
+        
+        string += "Fizz" if i % 3 == 0 else ''
+        string += "Buzz" if i % 5 == 0 else ''
+
+        print(string)
+
+
+# fizzbuzz()
+# happy_new_year() in looping.py prints 10 to 1 countdown then "Happy New Year!" F     [ 33%]
+# square_integers() in looping.py returns squared ints for [1, 2, 3, 4, 5] and [-1, -2, -3, -4, -5] F [ 66%]
+#                                                                                      [ 66%]
+# fizzbuzz() in looping.py prints 1 to 100 with fizz 3s, buzz 5s, fizzbuzz 3and5s F    [100%]
